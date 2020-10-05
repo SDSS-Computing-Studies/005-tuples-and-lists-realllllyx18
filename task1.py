71524e04ba38c0b9c87c2e8606443cef3f06e5ce
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-05 12:35:06 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -24,4 +24,7 @@ The animal at that index is Dog
 
 
 
-animals =
+animals = ["Cat", "Fish", "Dog", "Bear", "Turtle"]
+animals.sort()
+number=(input("Enter a number")).strip()
+print("The animal at that index is "+animals[int(number)])
